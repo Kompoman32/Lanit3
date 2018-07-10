bf81ac0aa394be2ba0d29b4264e101c8d008caca
Буляков Данил Радисович <bulyakov98@gmail.com>
2018-07-10 16:16:14 +0500

    Added database initialization

--- Lanit3/Global.asax.cs
+++ Lanit3/Global.asax.cs
@@ -18,6 +18,7 @@ namespace Lanit3
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Models.DataBase.Init();
         }
     }
 }

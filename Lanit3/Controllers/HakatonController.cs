e5e8f08a435ef57a36b8d4eda9b6bc9c9afc8c6d
Буляков Данил Радисович <bulyakov98@gmail.com>
2018-07-10 13:24:17 +0500

    HakatonController added, requests added

---
+++ Lanit3/Controllers/HakatonController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Lanit3.Models;
+
+namespace Lanit3.Controllers
+{
+    public class HakatonController : ApiController
+    {
+        [HttpPost]
+        [Route("person")]
+        public void Person([FromBody] Person person)
+        {
+            // save person
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("car")]
+        public void Car([FromBody] Car car)
+        {
+            // save car
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("personwithcars")]
+        public Person PersonWithCars(long personId)
+        {
+            // return person if exists
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("statistics")]
+        public Statistics Statistics()
+        {
+            // return statistics
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("clear")]
+        public void Clear()
+        {
+            // clear database
+            throw new NotImplementedException();
+        }
+    }
+}

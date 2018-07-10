bfe37ef38536ce558c51d9daf3187644141f3db7
Буляков Данил Радисович <bulyakov98@gmail.com>
2018-07-10 12:51:10 +0500

    EntityFramework installed, Person added

---
+++ Lanit3/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Lanit3.Models
+{
+    public class Person
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("birthdate")]
+        public string BirthDate { get; set; }
+    }
+}
